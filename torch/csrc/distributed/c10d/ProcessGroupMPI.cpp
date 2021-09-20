47bc9c4135e4a9e6cf4d037d9a25479ee9602a9b
Jason Dent <jason@katanagraph.com>
2021-09-20 23:27:25 +0000

    Changes thread support level check back to MPI_THREAD_SERIALIZED

--- torch/csrc/distributed/c10d/ProcessGroupMPI.cpp
+++ torch/csrc/distributed/c10d/ProcessGroupMPI.cpp
@@ -242,14 +242,15 @@ void ProcessGroupMPI::initMPIOnce() {
         TORCH_CHECK(false, "Fail to register the MPI exit handler");
       }
     }
-
-    if (mpiThreadSupport_ < MPI_THREAD_SERIALIZED) {
+/*
+    if (mpiThreadSupport_ < MPI_THREAD_MULTIPLE) {
       TORCH_CHECK(false,
 	    "Used MPI implementation doesn't have the "
 	    "minimum level of threading support: "
-	    "MPI_THREAD_SERIALIZED. This is required by "
+	    "MPI_THREAD_MULTIPLE. This is required by "
 	    "c10d package");
     }
+*/
   });
 }
 
